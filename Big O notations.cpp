eadbe488282813a746e4c6ccec3fd3fd102bef74
Tony <102136989+AntV3@users.noreply.github.com>
2022-10-12 12:11:41 -0700

    Big O notations example

---
+++ Big O notations.cpp
@@ -0,0 +1,28 @@
+// Big O Notation
+
+# include <iostream>
+
+using namespace std;
+
+int* test array;
+int arraysizes;
+
+// Big O: O(n)
+
+void DisplayArray() {
+    for (int i = 0; i <arraySize; i++) {
+        << cout << testArray[i] << endl;
+    }
+}
+
+int main() {
+
+    cout << "Enter array size" << endl;
+    cin >> arraySize;
+
+    testArray = new int[arraySize];
+
+
+
+delete[] testArray;
+}
